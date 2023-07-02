12d11e7302115258a6434b421e1ab2e1afef470d
Yash Paota <yashpaota@gmail.com>
2023-07-02 19:04:32 +0530

    my first commit

---
+++ src/components/DarkMode.js
@@ -0,0 +1,17 @@
+import React, { useState } from  "react";
+
+const DarkMode = () => {
+
+    const[darkMode , setDarkMode] = useState(false);
+    return(
+        <>
+           <div className={` page ${darkMode && "dark-mode"}`}>
+              <button className="dark-mode-button" onClick={() => setDarkMode(true)}>Dark Mode</button>
+              <button className="light-mode-button" onClick={() => setDarkMode(false)}>Light Mode</button>
+           </div>
+
+        </>
+    )
+};
+
+export default DarkMode;
